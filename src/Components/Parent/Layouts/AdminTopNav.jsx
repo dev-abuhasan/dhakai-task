badeee9ba72ee29fb439fada5a575b76913021aa
dev-abuhasan <ahinstitutte@gmail.com>
2022-02-08 23:25:12 +0600

    top nav done

--- src/Components/Parent/Layouts/AdminTopNav.jsx
+++ src/Components/Parent/Layouts/AdminTopNav.jsx
@@ -1,10 +1,40 @@
 import './LayoutScss/TopNav.scss';
+import { Icon } from '@material-ui/core';
 
 const AdminTopNav = () => {
 
     return (
-        <main id="mainTopNavbar" className="">
-            <h1>This is top nav</h1>
+        <main id="mainTopNavbar" className="d-flex align-items-center justify-content-between px-5">
+            <div className=''>
+                <div className='buttonGroup d-flex align-items-center'>
+                    <span className='btn explore'>Explore</span>
+                    <span className='btn'>Activity</span>
+                </div>
+            </div>
+            <div className='' id="navSrcBar">
+                <form>
+                    <div className='relative'>
+                        <input
+                            type='text'
+                            placeholder="Search by name, group, type and others"
+                        />
+                        <Icon>search</Icon>
+                    </div>
+                </form>
+            </div>
+            <div className='navRightIcons d-flex align-items-center'>
+                <div className='d-flex align-items-center justify-content-center'>
+                    <Icon>textsms</Icon>
+                    <span className='common'>2</span>
+                </div>
+                <div className='d-flex align-items-center justify-content-center'>
+                    <Icon>notifications</Icon>
+                    <span className='common'>6</span>
+                </div>
+                <div className='user d-flex align-items-center justify-content-center'>
+                    <Icon>account_circle</Icon>
+                </div>
+            </div>
         </main>
     );
 };
