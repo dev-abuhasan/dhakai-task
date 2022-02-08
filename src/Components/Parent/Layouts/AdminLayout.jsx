0b55e2d8b4cbbd1f556b1f0b866a7eab06cc2a6d
dev-abuhasan <ahinstitutte@gmail.com>
2022-02-09 00:20:36 +0600

    top nav issue fix with responsive

--- src/Components/Parent/Layouts/AdminLayout.jsx
+++ src/Components/Parent/Layouts/AdminLayout.jsx
@@ -1,13 +1,6 @@
-import React, { useState, useEffect, useContext } from "react";
-import {
-    makeStyles,
-    createStyles,
-    useTheme,
-} from "@material-ui/core/styles";
+import React, { useContext } from "react";
 import './LayoutScss/AdminLayout.scss';
 import Drawer from "@material-ui/core/Drawer";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import clsx from "clsx";
 import { renderRoutes } from "react-router-config";
 import AdminSideNavLayout from "./AdminSideNav";
 import { Icon } from "@material-ui/core";
@@ -18,132 +11,69 @@ import { logout } from "../../../services/redux/actions/userAction";
 import AdminTopNav from "./AdminTopNav";
 
 
+// 
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+
 const AdminLayout = () => {
     const dispatch = useDispatch();
     const { rootRouting } = useContext(AppContext);
     const drawerWidth = 70;
 
-    const useStyles = makeStyles((theme) =>
-        createStyles({
-            root: {
-                display: "flex",
-            },
-            appBar: {
-                transition: theme.transitions.create(["margin", "width"], {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.leavingScreen,
-                }),
-            },
-            appBarShift: {
-                width: `calc(100% - ${drawerWidth}px)`,
-                marginLeft: drawerWidth,
-                transition: theme.transitions.create(["margin", "width"], {
-                    easing: theme.transitions.easing.easeOut,
-                    duration: theme.transitions.duration.enteringScreen,
-                }),
-            },
-            menuButton: {
-                marginRight: theme.spacing(2),
-            },
-            hide: {
-                display: "none",
-            },
-            drawer: {
-                flexShrink: 0,
-            },
-            drawerHide: {
-                width: 0,
-            },
-            drawerPaper: {
-                width: drawerWidth,
-                backgroundColor: `#fff`,
-                color: `rgb(19, 17, 17)`
-            },
-            drawerHeader: {
-                display: "flex",
-                alignItems: "center",
-                padding: theme.spacing(0, 1),
-                // necessary for content to be below app bar
-                ...theme.mixins.toolbar,
-                justifyContent: "flex-end",
-            },
-            content: {
-                flexGrow: 1,
-                padding: theme.spacing(3),
-                transition: theme.transitions.create("margin", {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.leavingScreen,
-                }),
-            },
-            contentShift: {
-                transition: theme.transitions.create("margin", {
-                    easing: theme.transitions.easing.easeOut,
-                    duration: theme.transitions.duration.enteringScreen,
-                }),
-                marginLeft: drawerWidth,
-            },
-        })
-    );
-    const classes = useStyles();
-    const theme = useTheme();
-    const smallScreen = useMediaQuery(theme.breakpoints.down("md"));
-    const [isSidebarOpen, setSidebarOpen] = useState(!smallScreen);
-
-    useEffect(() => {
-        setSidebarOpen(!smallScreen);
-    }, [smallScreen]);
-
-
     return (
-        <div className={classes.root}>
-            <Drawer
-                id="sideNavDrawer"
-                className={clsx(classes.drawer, {
-                    [classes.drawerHide]: !isSidebarOpen,
-                })}
-                variant="permanent"
-                anchor="left"
-                open={isSidebarOpen}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={`my-2 ${classes.drawerHeader}`} >
-                    <div className="NavLogoParent mx-auto" style={{ width: "45px", height: "45px" }}>
-                        <img className="w-100" src="https://i.ibb.co/3cfhM24/icon-192x192.png" alt="" />
+        <div className="">
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <Drawer
+                    id="sideNavDrawer"
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                    variant="permanent"
+                    anchor="left"
+                >
+                    <div className={`my-2 `} >
+                        <div className="NavLogoParent mx-auto" style={{ width: "45px", height: "45px" }}>
+                            <img className="w-100" src="https://i.ibb.co/3cfhM24/icon-192x192.png" alt="" />
+                        </div>
                     </div>
-                </div>
-                <AdminSideNavLayout />
-                <div className="navBottom">
-                    <button className="btn">
-                        <Icon className="DEFAULT_COLOR">settings</Icon>
-                    </button>
-                    <button className="btn">
-                        <Icon className="DEFAULT_COLOR">help_outline</Icon>
-                    </button>
-                    <div className=""
-                        onClick={() => {
-                            dispatch(logout());
-                        }}
-                    >
+                    <AdminSideNavLayout />
+                    <div className="navBottom d-flex flex-column">
                         <button className="btn">
-                            <Icon className="DEFAULT_COLOR">logout</Icon>
+                            <Icon className="DEFAULT_COLOR">settings</Icon>
                         </button>
+                        <button className="btn">
+                            <Icon className="DEFAULT_COLOR">help_outline</Icon>
+                        </button>
+                        <div className=""
+                            onClick={() => {
+                                dispatch(logout());
+                            }}
+                        >
+                            <button className="btn">
+                                <Icon className="DEFAULT_COLOR">logout</Icon>
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </Drawer>
-            <main
-                className={`p-0 w-100 ${clsx(classes.content, {
-                    [classes.contentShift]: isSidebarOpen && !smallScreen,
-                })}`}
-            >
-                <AdminTopNav />
-                <div className="main_section">
-                    <LoadingSuspense>
-                        {renderRoutes(rootRouting)}
-                    </LoadingSuspense>
-                </div>
-            </main>
+                </Drawer>
+                <Box
+                    component="main"
+                    className="w-100"
+                    sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `57px` }}
+                >
+                    <AdminTopNav />
+                    <div className="main_section">
+                        <LoadingSuspense>
+                            {renderRoutes(rootRouting)}
+                        </LoadingSuspense>
+                    </div>
+                </Box>
+            </Box>
         </div>
     );
 }
--- src/Components/Parent/Layouts/AdminTopNav.jsx
+++ src/Components/Parent/Layouts/AdminTopNav.jsx
@@ -1,40 +1,50 @@
 import './LayoutScss/TopNav.scss';
 import { Icon } from '@material-ui/core';
+import { Nav, Navbar } from 'react-bootstrap';
 
 const AdminTopNav = () => {
 
     return (
-        <main id="mainTopNavbar" className="d-flex align-items-center justify-content-between px-5">
-            <div className=''>
-                <div className='buttonGroup d-flex align-items-center'>
-                    <span className='btn explore'>Explore</span>
-                    <span className='btn'>Activity</span>
-                </div>
-            </div>
-            <div className='' id="navSrcBar">
-                <form>
-                    <div className='relative'>
-                        <input
-                            type='text'
-                            placeholder="Search by name, group, type and others"
-                        />
-                        <Icon>search</Icon>
-                    </div>
-                </form>
-            </div>
-            <div className='navRightIcons d-flex align-items-center'>
-                <div className='d-flex align-items-center justify-content-center'>
-                    <Icon>textsms</Icon>
-                    <span className='common'>2</span>
-                </div>
-                <div className='d-flex align-items-center justify-content-center'>
-                    <Icon>notifications</Icon>
-                    <span className='common'>6</span>
-                </div>
-                <div className='user d-flex align-items-center justify-content-center'>
-                    <Icon>account_circle</Icon>
-                </div>
-            </div>
+        <main id="mainTopNavbar" className="px-5">
+            <Navbar expand="lg" className="main_nav_parent d-flex justify-content-end p-0">
+                <Navbar.Toggle className='' aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="w-100">
+                        <div className='w-100 lgScreen d-flex align-items-center justify-content-between '>
+                            <div className=''>
+                                <div className='buttonGroup d-flex align-items-center'>
+                                    <span className='btn explore'>Explore</span>
+                                    <span className='btn'>Activity</span>
+                                </div>
+                            </div>
+                            <div className='' id="navSrcBar">
+                                <form>
+                                    <div className='position-relative'>
+                                        <input
+                                            type='text'
+                                            placeholder="Search by name, group, type and others"
+                                        />
+                                        <Icon>search</Icon>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className='navRightIcons d-flex align-items-center'>
+                                <div className='d-flex align-items-center justify-content-center'>
+                                    <Icon>textsms</Icon>
+                                    <span className='common'>2</span>
+                                </div>
+                                <div className='d-flex align-items-center justify-content-center'>
+                                    <Icon>notifications</Icon>
+                                    <span className='common'>6</span>
+                                </div>
+                                <div className='user d-flex align-items-center justify-content-center'>
+                                    <Icon>account_circle</Icon>
+                                </div>
+                            </div>
+                        </div>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </main>
     );
 };
